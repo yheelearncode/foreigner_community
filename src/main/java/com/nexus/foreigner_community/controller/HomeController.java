22a5a2fbb3942f764bdef7cef40649a62d2175eb
yheelearncode <yonghi01@naver.com>
2025-09-15 23:15:00 +0900

    thymeleaf test

--- src/main/java/com/nexus/foreigner_community/controller/HomeController.java
+++ src/main/java/com/nexus/foreigner_community/controller/HomeController.java
@@ -9,13 +9,11 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "외국인 커뮤니티");
         return "index";
     }
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "소개");
         return "about";
     }
 
