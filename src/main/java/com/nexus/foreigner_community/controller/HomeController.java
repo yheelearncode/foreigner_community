28006f4fa2c56a209483ad801171057f7cef893a
gawon04 <kgw0550@naver.com>
2025-10-31 13:37:44 +0900

    Add frontend code to map branch

---
+++ src/main/java/com/nexus/foreigner_community/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.nexus.foreigner_community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "about";
+    }
+
+
+}
---
+++ src/test/java/com/nexus/foreigner_community/ForeignerCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.foreigner_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignerCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
